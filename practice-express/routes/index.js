408a02f934330fdb3849f1203a24c7d664ced2c8
Harjeevan Tanda <harjeevan@hangarbot.com>
2019-12-04 23:57:57 -0500

    code cleanup

--- practice-express/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-
-routes.use('/app', require('./../app'));
-
-routes.post('/login', (request, response)=>{
-    response.send("Login method in file index.js");
-});
-module.exports = routes
